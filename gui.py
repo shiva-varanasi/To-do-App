7cb5b6f87d42cf1b375f697a5ae575d7e1162031
shiva <shiva.3724@gmail.com>
2024-01-27 20:58:06 +0100

    fix typo errors, day 16

--- gui.py
+++ gui.py
@@ -5,6 +5,6 @@ label = sg.Text("Enter a to-do")
 input_box = sg.InputText(tooltip = "Enter todo")
 add_button = sg.Button("Add")
 
-window = sg.Window("My Todo Appdfggdf", layout = [[label], [input_box, add_button]])
+window = sg.Window("My Todo App", layout = [[label], [input_box, add_button]])
 window.read()
 window.close()
