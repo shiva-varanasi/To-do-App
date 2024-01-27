53f7ed0341d533fc59bce85bb6433f8b31970c35
shiva <shiva.3724@gmail.com>
2024-01-27 21:05:05 +0100

    fix formating issues, day 16

--- gui.py
+++ gui.py
@@ -2,7 +2,7 @@ import functions
 import PySimpleGUI as sg
 
 label = sg.Text("Enter a to-do")
-input_box = sg.InputText(tooltip = "Enter todo")
+input_box = sg.InputText(tooltip="Enter todo")
 add_button = sg.Button("Add")
 
 window = sg.Window("My Todo App", layout=[[label], [input_box, add_button]])
