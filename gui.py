dc3f932f6b0b4e041e816e578c7165c07baa3ce9
shiva <shiva.3724@gmail.com>
2024-02-04 19:16:37 +0100

    Handle creation of todos.txt file if not present already

--- gui.py
+++ gui.py
@@ -1,11 +1,16 @@
 import functions
 import PySimpleGUI as sg
 import time
+import os
+
+if not os.path.exists('todos.txt'):
+    with open('todos.txt', 'w') as file:
+        pass
 
 clock = sg.Text('', key='clock')
 label = sg.Text("Enter a to-do")
 input_box = sg.InputText(tooltip="Enter todo", key="todo")
-add_button = sg.Button("Add")
+add_button = sg.Button("Add", mouseover_colors='LightBlue2')
 list_box = sg.Listbox(values=functions.get_todos(), key="todos",
                       enable_events=True, size=[45, 10])
 edit_button = sg.Button("Edit")
